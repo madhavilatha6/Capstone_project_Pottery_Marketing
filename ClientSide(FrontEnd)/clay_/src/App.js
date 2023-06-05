6be0221b80a233c44d98c2bf0c0b4181b407f2ab
madhavilatha6 <madhavilathamadhu54@gmail.com>
2023-06-06 03:00:26 +0530

    feat: create home page

---
+++ ClientSide(FrontEnd)/clay_/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Images from './components/Images';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Images />
+      
+    </div>
+  );
+}
+
+export default App;
