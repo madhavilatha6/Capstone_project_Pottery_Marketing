e9087655c41ffea3804863ef09ce3c1d9fc7966d
madhavilatha6 <madhavilathamadhu54@gmail.com>
2023-06-08 20:20:45 +0530

    feat: remaining homepage created

--- ClientSide(FrontEnd)/clay_/src/App.js
+++ ClientSide(FrontEnd)/clay_/src/App.js
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 import './App.css';
-// import Forgot from './components/forgot';
-// import Images from './components/Images';
+import Forgot from './components/forgot';
+import Images from './components/Images';
 import Login from './components/login';
 import Signup from './components/signup';
 
 function App() {
-  const [islogin,setIsLogin] = useState(false);
   return (
     <div className="App">
-      {/* <Images /> */}
-      {
-         (!islogin) ? <Signup islogin={islogin} setIsLogin={setIsLogin}/> : <Login islogin={islogin} setIsLogin={setIsLogin}/>
-      }
-      {/* <Forgot /> */}
+      <Images />
+     <Login />
+     <Signup />
+      <Forgot />
     </div>
   );
 }
---
+++ ClientSide(FrontEnd)/clay_/src/components/Images.js
@@ -0,0 +1,194 @@
+import './Images.css';
+ import img1 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 15-00-41.png';
+ import img2 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 15-39-02.png';
+ import img3 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 15-39-54.png';
+ import img4 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 15-40-23.png';
+ import img5 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 15-38-40.png';
+ import img6 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/image.jpg';
+ import img7 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-05 19-53-44.png';
+ import img8 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-05 19-53-25.png';
+ import img9 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/download.png';
+ import img10 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/dolor.png';
+ import img11 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/bell2.jpg';
+ import img12 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/bell2.jpg';
+ import img13 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/OTP-METAL25.webp';
+ import img14 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-06 00-21-36.png'
+ import img15 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 01-30-52.png'
+ import img16 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 01-31-22.png'
+ import img17 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 01-32-09.png'
+ import img18 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 01-34-18.png'
+ import img19 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 01-54-32.png'
+ import img20 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/pot1.png'
+ import img21 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/pot2.png'
+ import img22 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/pot3.png'
+ import img23 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/pot4.png'
+ import img24 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-08 15-55-44.png';
+function Images(){
+
+    return(
+        <div>
+            <div>
+                <img className="img1" src={img1} alt=""></img>
+                <div className="nav">
+                    <div>Home</div>
+                    <div>Category</div>
+                    <div>About Us</div>
+                    <button className="btn">Register</button>
+                    <button className="btn">Add to cart</button>
+                </div>
+            <div className='smallpot'>
+                    <div className='picture'>
+                        <img className="img20" src={img20} alt=""></img>
+                        <h2>Pottery Classes</h2>
+                        <div className='ptag1'>
+                        <p>Integer condime condimentum </p><p>condimented dsemeu u finibus</p><p>portturabitur imperdiet.</p>
+                        </div>
+                    </div>
+                    <div className='picture1'>
+                        <img className="img21" src={img21} alt=""></img>
+                        <h2>Design Process</h2>
+                        <div className='ptag1'>
+                        <p>Integer condime condimentum </p><p>condimented dsemeu u finibus</p><p>portturabitur imperdiet.</p>
+                        </div>
+                    </div>
+                    <div className='picture2'>
+                        <img className="img22" src={img22} alt=""></img>
+                        <h2>Gift Cards</h2>
+                        <div className='ptag1'>
+                        <p>Integer condime condimentum </p><p>condimented dsemeu u finibus</p><p>portturabitur imperdiet.</p>
+                        </div>
+                    </div>
+                    <div className='picture3'>
+                        <img className="img23" src={img23} alt=""></img>
+                        <h2>Pottery Wheel</h2>
+                        <div className='ptag1'>
+                        <p>Integer condime condimentum </p><p>condimented dsemeu u finibus</p><p>portturabitur imperdiet.</p>
+                        </div>
+                    </div>
+
+            </div>
+
+
+
+            </div>
+            <div className="box">
+                <div className="second">
+                    <div className="image">
+                        <img className="img2" src={img2} alt=""></img>
+                        <p>plain Flower pot  </p>
+                    </div> 
+                    <div className="image">
+                        <img className="img3" src={img3} alt=""></img>
+                        <p>Design Flower pot  </p>
+                    </div>
+                    <div className="image">
+                        <img className="img4" src={img4} alt=""></img>
+                        <p>stand Flower pot </p>
+                    </div>
+                    <div className="image">
+                        <img className="img5" src={img5} alt=""></img>
+                        <p>model Flower pot  </p>
+                    </div> 
+                   </div>
+            </div>
+            <div className='hangingbell'>
+                <div>
+                    <img className="img24" src={img24} alt=""></img>
+                </div>
+                <div>
+                    <img className="img25" src={img24} alt=""></img>
+                </div>
+            </div>
+            <div>
+                <div className="nextpage">
+                    <div className="inside"> <img className="img6" src={img6} alt=""></img></div>
+                </div>
+                <div className="nextpage1">
+                    <p>About Us</p>
+                    <p>***</p>
+                    <p>We are a global housewares product design company.<br></br> We bring thought and creativity to everyday items<br></br> through original design.</p>
+                    <button>Shop Now</button>
+                </div>
+            </div>
+            <div className="icons">
+                <div>
+                    <img className="img7" src={img7} alt=""></img>
+                    <p className="ptag">FREE SHIPPING</p>
+               </div>
+               <div>
+                    <img className="img8" src={img8} alt=""></img>
+                    <p className="ptag">Item Return & Exchange</p>
+               </div>
+               <div>
+                    <img className="img9" src={img9} alt=""></img>
+                    <p className="ptag">Worldwide Free Shipping</p>
+               </div>
+               <div>
+                    <img className="img10" src={img10} alt=""></img>
+                    <p className="ptag">24/7 ONLINE SUPPORT</p>
+               </div>
+            </div>
+            <div className="maindiv">
+                <div className="firstdiv">
+                    <img className="img11" src={img11} alt=""></img>
+                    <img className="img13" src={img13} alt=""></img>
+                    
+                </div>
+                <div className="seconddiv">
+                     <img className="img12" src={img12} alt=""></img>
+                     
+
+                </div>
+            </div>
+            <div className="middlebox">
+                 
+                     <img className="img14" src={img14} alt=""></img>
+                <div className="alltext">
+                    <p>Welcome to</p>
+                    <b>Pottery Shop</b>
+                    <p>Pottery is the ceramic material which makes up potteryware.</p>
+                    <button>ReadMore</button>
+                </div> 
+
+            </div>
+            <div className='footer1'>
+                    <div className='about'>
+                        <h4>AboutUs</h4>
+                        <p>Potters</p>
+                        <p>Vision</p>
+                        <p>Aim</p>
+                    </div>
+                    <div className='service'>
+                        <h4>Services</h4>
+                        <p>Writing</p>
+                        <p>Vision</p>
+                        <p>Aim</p>
+                    </div>
+                    <div className='contact'>
+                        <h4>Contactus</h4>
+                        <p>Andrapradesh</p>
+                        <p>Karnataka</p>
+                        <p>Tamilnadu</p>
+                    </div>
+                    <div className='social'>
+                        <h4>Socialmedia</h4>
+                        <p>Facebook</p>
+                        <p>WhatsApp</p>
+                        <p>Twitter</p>
+                        <p>Google</p>
+                        <p>Instagram</p>
+                    </div>
+                    <div className='logo'>
+                    <img className="img15" src={img15} alt=""></img>
+                    <img className="img16" src={img16} alt=""></img>
+                    <img className="img17" src={img17} alt=""></img>
+                    <img className="img18" src={img18} alt=""></img>
+                    </div>
+                    <div>
+                    <img className="img19" src={img19} alt=""></img>
+                    </div>  
+            </div>
+     </div>
+    )
+}
+export default Images;
