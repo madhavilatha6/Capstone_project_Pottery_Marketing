1e57849abbd5534a40d32166e69e638fd7c15cb4
madhavilatha6 <madhavilathamadhu54@gmail.com>
2023-06-08 20:04:14 +0530

    feat: add home page

---
+++ ClientSide(FrontEnd)/clay_/src/components/Images.jsx
@@ -0,0 +1,106 @@
+import './Images.css';
+ import img1 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/components/home.jpg';
+ import img2 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/components/WhatsApp Image 2023-05-30 at 5.09.49 PM(2).jpeg';
+ import img3 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/WhatsApp Image 2023-05-30 at 5.09.36 PM.jpeg';
+ import img4 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/WhatsApp Image 2023-05-30 at 5.09.46 PM(1).jpeg';
+ import img5 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/components/WhatsApp Image 2023-05-30 at 5.09.33 PM(2).jpeg';
+ import img6 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/image.jpg';
+ import img7 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-05 19-53-44.png';
+ import img8 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-05 19-53-25.png';
+ import img9 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/download.png';
+ import img10 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/dolor.png';
+ import img11 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/bell2.jpg';
+ import img12 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/bell2.jpg';
+ import img13 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/OTP-METAL25.webp';
+ import img14 from '/home/jtbbjoining/Pottery_Marketing/ClientSide(FrontEnd)/clay_/src/Screenshot from 2023-06-06 00-21-36.png'
+function Images(){
+
+    return(
+        <div>
+            <div>
+                <img className="img1" src={img1} alt=""></img>
+                <div className="nav">
+                    <div>Home</div>
+                    <div>Category</div>
+                    <div>About Us</div>
+                    <button className="btn">Register</button>
+                    <button className="btn">Add to cart</button>
+                </div>
+            
+            </div>
+            <div className="box">
+                <div className="second">
+                    <div className="image">
+                        <img className="img2" src={img2} alt=""></img>
+                        <p>plain Flower pot  </p>
+                    </div> 
+                    <div className="image">
+                        <img className="img3" src={img3} alt=""></img>
+                        <p>Design Flower pot  </p>
+                    </div>
+                    <div className="image">
+                        <img className="img4" src={img4} alt=""></img>
+                        <p>stand Flower pot </p>
+                    </div>
+                    <div className="image">
+                        <img className="img5" src={img5} alt=""></img>
+                        <p>model Flower pot  </p>
+                    </div> 
+                </div>
+            </div>
+            <div>
+                <div className="nextpage">
+                    <div className="inside"> <img className="img6" src={img6} alt=""></img></div>
+                </div>
+                <div className="nextpage1">
+                    <p>About Us</p>
+                    <p>*********</p>
+                    <p>We are a global housewares product design company.<br></br> We bring thought and creativity to everyday items<br></br> through original design.</p>
+                    <button>Shop Now</button>
+                </div>
+            </div>
+            <div className="icons">
+                <div>
+                    <img className="img7" src={img7} alt=""></img>
+                    <p className="ptag">FREE SHIPPING</p>
+               </div>
+               <div>
+                    <img className="img8" src={img8} alt=""></img>
+                    <p className="ptag">Item Return & Exchange</p>
+               </div>
+               <div>
+                    <img className="img9" src={img9} alt=""></img>
+                    <p className="ptag">Worldwide Free Shipping</p>
+               </div>
+               <div>
+                    <img className="img10" src={img10} alt=""></img>
+                    <p className="ptag">24/7 ONLINE SUPPORT</p>
+               </div>
+            </div>
+            <div className="maindiv">
+                <div className="firstdiv">
+                    <img className="img11" src={img11} alt=""></img>
+                    <img className="img13" src={img13} alt=""></img>
+                    
+                </div>
+                <div className="seconddiv">
+                     <img className="img12" src={img12} alt=""></img>
+                     
+
+                </div>
+            </div>
+            <div className="middlebox">
+                 
+                     <img className="img14" src={img14} alt=""></img>
+                <div className="alltext">
+                    <p>Welcome to</p>
+                    <b>Pottery Shop</b>
+                    <p>Pottery is the ceramic material which makes up potteryware.</p>
+                    <button>ReadMore</button>
+                </div> 
+
+            </div>
+     </div>
+    )
+}
+export default Images;
