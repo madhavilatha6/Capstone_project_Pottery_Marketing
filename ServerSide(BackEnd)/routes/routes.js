d609df92f36278a5d6a9c0b51ec086f47a017bbb
madhavilatha6 <madhavilathamadhu54@gmail.com>
2023-11-05 21:24:40 +0530

    AllProducts (frontend,backend) completed

--- ServerSide(BackEnd)/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { insertpotteryData,potterysearch,potterysort } from "../controllers/control.js"
-
-
-app.get('/potterdata',insertpotteryData)
-
-app.get('/search/:searchData',potterysearch)
-
-app.get('/sort/:sortData',potterysort)
-
